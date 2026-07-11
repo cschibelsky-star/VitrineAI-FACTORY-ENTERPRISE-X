e3bb6750e6be04bb4b18026f4e77e1828b051a32
cschibelsky-star <cschibelsky@gmail.com>
2026-07-11 20:05:30 -0300

    Adiciona exemplo seguro de credenciais do Guia Digital

---
+++ products/guia-digital-turismo/implementacao_4_3/config/credentials.local.example.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copie este arquivo para credentials.local.php e substitua o hash.
+ * Gere o hash no terminal com:
+ * php -r "echo password_hash('SUA_SENHA_FORTE', PASSWORD_DEFAULT), PHP_EOL;"
+ */
+return [
+    'user' => 'admin',
+    'password_hash' => 'COLE_AQUI_O_HASH_GERADO',
+];
