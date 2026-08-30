feeedc0e95048ca6225401b626e7a1297c971d10
cschibelsky-star <cschibelsky@gmail.com>
2026-08-30 15:21:29 -0300

    feat(cadastro): add module configuration

---
+++ Modules/Cadastro/config/cadastro.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'module_key' => 'cadastro',
+    'public_registration' => [
+        'enabled' => false,
+    ],
+    'print' => [
+        'show_branding' => true,
+        'require_signature' => true,
+    ],
+];
