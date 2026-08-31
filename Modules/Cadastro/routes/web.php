e1f91e4b0bdd46445e717a0b5f319d75b5f431b4
cschibelsky-star <cschibelsky@gmail.com>
2026-08-30 21:39:42 -0300

    feat(cadastro): wire people CRUD routes and permissions

--- Modules/Cadastro/routes/web.php
+++ Modules/Cadastro/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Cadastro\Http\Controllers\PersonController;
 use Modules\Cadastro\Http\Controllers\PersonPrintController;
 use Modules\Cadastro\Http\Controllers\PublicRegistrationController;
 
@@ -8,9 +9,21 @@ Route::middleware(['web', 'module:cadastro'])
     ->prefix('cadastro')
     ->name('cadastro.')
     ->group(function (): void {
-        Route::view('/', 'cadastro::index')
+        Route::get('/', [PersonController::class, 'index'])
             ->middleware('permission:cadastro.view')
             ->name('index');
+
+        Route::post('/pessoas', [PersonController::class, 'store'])
+            ->middleware('permission:cadastro.create')
+            ->name('people.store');
+
+        Route::put('/pessoas/{person}', [PersonController::class, 'update'])
+            ->middleware('permission:cadastro.update')
+            ->name('people.update');
+
+        Route::delete('/pessoas/{person}', [PersonController::class, 'destroy'])
+            ->middleware('permission:cadastro.delete')
+            ->name('people.destroy');
 
         Route::get('/pessoas/{person}/imprimir', PersonPrintController::class)
             ->middleware('permission:cadastro.print')
