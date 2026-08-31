5ddac6d0b5b06e6712e720f93421391c47fa8ade
cschibelsky-star <cschibelsky@gmail.com>
2026-08-30 21:26:42 -0300

    feat(cadastro): expose public registration and print routes

--- Modules/Cadastro/routes/web.php
+++ Modules/Cadastro/routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Cadastro\Http\Controllers\PrintPersonController;
+use Modules\Cadastro\Http\Controllers\PublicRegistrationController;
 
 Route::middleware(['web', 'module:cadastro'])
     ->prefix('cadastro')
     ->name('cadastro.')
     ->group(function (): void {
         Route::view('/', 'cadastro::index')->name('index');
+
+        Route::get('/publico', [PublicRegistrationController::class, 'create'])
+            ->name('public.create');
+        Route::post('/publico', [PublicRegistrationController::class, 'store'])
+            ->name('public.store');
+
+        Route::get('/pessoas/{person}/imprimir', PrintPersonController::class)
+            ->name('people.print');
     });
