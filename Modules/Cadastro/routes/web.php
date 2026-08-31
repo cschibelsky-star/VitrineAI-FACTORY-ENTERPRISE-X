2622ec1020ed505d9c06ea6ab0aa19418e8e769e
cschibelsky-star <cschibelsky@gmail.com>
2026-08-30 21:38:20 -0300

    fix(cadastro): enforce operational permissions on web routes

--- Modules/Cadastro/routes/web.php
+++ Modules/Cadastro/routes/web.php
@@ -1,20 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Cadastro\Http\Controllers\PrintPersonController;
+use Modules\Cadastro\Http\Controllers\PersonPrintController;
 use Modules\Cadastro\Http\Controllers\PublicRegistrationController;
 
 Route::middleware(['web', 'module:cadastro'])
     ->prefix('cadastro')
     ->name('cadastro.')
     ->group(function (): void {
-        Route::view('/', 'cadastro::index')->name('index');
+        Route::view('/', 'cadastro::index')
+            ->middleware('permission:cadastro.view')
+            ->name('index');
+
+        Route::get('/pessoas/{person}/imprimir', PersonPrintController::class)
+            ->middleware('permission:cadastro.print')
+            ->name('people.print');
 
         Route::get('/publico', [PublicRegistrationController::class, 'create'])
             ->name('public.create');
         Route::post('/publico', [PublicRegistrationController::class, 'store'])
             ->name('public.store');
-
-        Route::get('/pessoas/{person}/imprimir', PrintPersonController::class)
-            ->name('people.print');
     });
